ddd43564c67fcfd235ea9da7e2011b9fbfcbca44
Zohre Salimi <zohre.1987@gmail.com>
2020-12-01 17:37:25 +0100

    update test snapshot

--- src/pages/Booking/index.js
+++ src/pages/Booking/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect } from "react";
 import AppContext from "../../store/context";
 import TopBar from "../../components/TopBar";
